b18d14a862065cfa687bb01aa0073e1176a75d6f
Mert <nagodretrem@gmail.com>
2022-12-20 00:31:53 +0300

    Middleware concept and template engine using

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
+
+//Template Engine
+app.set('view engine', 'ejs');
+
 //Middleware
 app.use(express.static('public'));
 
-const port = 3000;
-
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
