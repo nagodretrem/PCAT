118adc58f8a0e9ac3c5b45c0943a2c5655d779a6
Mert <nagodretrem@gmail.com>
2022-12-22 11:51:25 +0300

    Single photo page created. Required route is created.

--- app.js
+++ app.js
@@ -15,13 +15,18 @@ app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //Routes
 app.get('/', async (req, res) => {
   const photos = await Photo.find();
   res.render('index', { photos });
+});
+app.get('/photos/:id', async (req, res) => {
+  //console.log(req.params.id);
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', { photo });
 });
 app.get('/about', (req, res) => {
   res.render('about');
