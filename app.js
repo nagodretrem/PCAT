331740964e61265bdf0fb4bed5bd5344c9dc4af0
Mert <nagodretrem@gmail.com>
2022-12-22 15:52:19 +0300

    Added photo upload function.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const conn = require('./db');
+const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
+const ejs = require('ejs');
+const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 dotenv.config();
@@ -17,32 +21,47 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //Routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find();
+  const photos = await Photo.find().sort({ dateCreated: -1 });
   res.render('index', { photos });
 });
+
 app.get('/photos/:id', async (req, res) => {
-  //console.log(req.params.id);
   const photo = await Photo.findById(req.params.id);
   res.render('photo', { photo });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
+
 app.get('/add', (req, res) => {
   res.render('add');
 });
+
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadedImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadedImage.name;
+
+  uploadedImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadedImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-/* const ejs = require('ejs');
-const path = require('path'); */
