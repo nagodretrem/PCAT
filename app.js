b7f687869d1389b765b17ab239d1821cc53a3275
Mert <nagodretrem@gmail.com>
2022-12-22 16:23:21 +0300

    Added function to update photo information

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conn = require('./db');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const dotenv = require('dotenv');
 const ejs = require('ejs');
 const path = require('path');
@@ -22,6 +23,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 //Routes
 app.get('/', async (req, res) => {
@@ -59,6 +61,24 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findById({
+    _id: req.params.id,
+  });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  await photo.save();
+
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = process.env.PORT;
