b7606648d3f600b57b138b11c77ed723f97d487c
Mert <nagodretrem@gmail.com>
2022-12-23 14:26:16 +0300

    Added pagination to the project.

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -2,8 +2,23 @@ const Photo = require('../models/Photo');
 const fs = require('fs');
 
 exports.getAllPhotos = async (req, res) => {
-  const photos = await Photo.find().sort({ dateCreated: -1 });
-  res.render('index', { photos });
+  const page = req.query.page || 1;
+  const photosPerPage = 3;
+  const totalPhotos = await Photo.countDocuments();
+
+  const photos = await Photo.find()
+    .sort({ dateCreated: -1 })
+    .skip((page - 1) * photosPerPage)
+    .limit(photosPerPage);
+
+  res.render('index', {
+    photos: photos,
+    current: page,
+    pages: Math.ceil(totalPhotos / photosPerPage),
+  });
+
+  /*   const photos = await Photo.find().sort({ dateCreated: -1 });
+  res.render('index', { photos }); */
 };
 
 exports.getPhoto = async (req, res) => {
