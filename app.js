89647c3252259e0f7ad39c45280250d43102c799
Mert <nagodretrem@gmail.com>
2022-12-23 16:06:42 +0300

    Complete project

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.get('/about', pageController.getAboutPage);
 app.get('/add', pageController.getAddPage);
 app.get('/photos/edit/:id', pageController.getEditPage);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
